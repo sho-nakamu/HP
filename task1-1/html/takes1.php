df7cdc3ff4fb58e643a08fcb85e2dc22ee790265
sho-nakamu <nshogo0317@gmail.com>
2024-01-17 22:47:40 +0900

    task1.完成

---
+++ task1-1/html/takes1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  暑中お見舞い申し上げます。<br>
+  暑中お見舞い<br>
+  申し上げます。
+</body>
+</html>
